12f185512600a498b5fcce74068b2303d7d1dd2f
josimarsousa <josimarsousa@gmx.com>
2026-01-01 21:02:41 -0300

    variables

---
+++ variables/variables.py
@@ -0,0 +1,21 @@
+#nao se pode usar variveis começando por números 
+# e espaços entre palavras
+a1 = 100; # ok
+velociade = 100; # ok
+salario_medio = 2500.00; # ok
+
+#variaveis numericas
+a = 1
+b = 2
+c = 3
+d = 4
+e = 5
+
+a == b
+False   
+
+b > a
+True
+
+b >= a
+True
