e46eb0e21a1657ae2fc2670a366302a6c84f2cfc
josimarsousa <josimarsousa@gmx.com>
2026-01-01 21:39:15 -0300

    strings

---
+++ variables/strings.py
@@ -0,0 +1,46 @@
+nome = "fulano"
+
+print(nome)
+fulano
+
+len(nome)
+6
+
+len("O rato roeu a roupa ")
+
+
+#acessando um indice em uma palavra
+
+a = "ABCDEF"
+
+a[0]
+A
+
+a[1]
+B
+
+a[3]
+D
+
+a[4]
+E
+
+a[5]
+F
+
+#concatenaçao
+a + "G"
+'ABCDEFG'
+
+a + "H" * 3
+'ABCDEFGHHH'
+
+
+
+
+
+
+
+
+
+
