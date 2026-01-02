58fecc6d8589b466041b3fc4f98f25705e7b14c7
josimarsousa <josimarsousa@gmx.com>
2026-01-01 21:10:51 -0300

    operadores logicos

--- variables/variables.py
+++ variables/variables.py
@@ -1,5 +1,8 @@
 #nao se pode usar variveis começando por números 
 # e espaços entre palavras
+from operator import truediv
+
+
 a1 = 100; # ok
 velociade = 100; # ok
 salario_medio = 2500.00; # ok
@@ -19,3 +22,51 @@ True
 
 b >= a
 True
+
+#operadores logicos
+nota = 9
+media = 7
+
+aprovado = nota >= media
+print(aprovado)
+True
+
+reprovado = nota < media
+print(reprovado)
+False
+
+#operadores logicos not, and, or
+#not
+not true
+False
+
+not false
+True
+
+#and
+True and True
+True
+
+True and False
+False
+
+False and True
+False
+
+False and False
+False
+
+#or
+True or True
+True
+
+True or False
+True    
+
+False or True
+True
+
+False or False
+False
+
+
