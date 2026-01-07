19adf5d25b3ba5696f36ecabf9c824769a5ab559
josimarsousa <josimarsousa@gmx.com>
2026-01-06 22:05:42 -0300

    exemplo tabuada

--- Fundamentos/controlesFluxo/condicionais_while.py
+++ Fundamentos/controlesFluxo/condicionais_while.py
@@ -3,3 +3,19 @@ x = 1
 while x <=10:
     print(x)
     x += 2
+
+
+#while e contadores
+fim = int(input("Digite o valor final: "))
+x = 0
+while x <= fim:
+    if x % 2 == 0:
+        print(x)
+    x += 1
+
+#while e tabuada
+tabuada = int(input("Digite o valor da tabuada: "))
+x = 1
+while x <= 10:
+    print(f"{tabuada} x {x} = {tabuada * x}")
+    x += 1
