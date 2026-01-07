0d6eda25592e1263efc6b0751baf888e03b89f92
josimarsousa <josimarsousa@gmx.com>
2026-01-07 10:06:19 -0300

    acumuladores e contadores

--- Fundamentos/controlesFluxo/condicionais_while.py
+++ Fundamentos/controlesFluxo/condicionais_while.py
@@ -19,3 +19,12 @@ x = 1
 while x <= 10:
     print(f"{tabuada} x {x} = {tabuada * x}")
     x += 1
+
+#acumuladores e contadores
+n = 1
+soma = 0
+while n <= 10:
+    x = int(input(f"Digite o {n} número:"))
+    soma = soma + x
+    n = n + 1
+print(f"Soma: {soma}")
