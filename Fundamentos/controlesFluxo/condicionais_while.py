a73f804a3ad37eaff0e2b04a1805f8ca9ffe88e6
josimarsousa <josimarsousa@gmx.com>
2026-01-07 10:23:07 -0300

    contagem de cedulas

--- Fundamentos/controlesFluxo/condicionais_while.py
+++ Fundamentos/controlesFluxo/condicionais_while.py
@@ -39,7 +39,7 @@ while x <= 5:
     soma = soma + n
     x = x + 1
 print(f"Média: {soma / 5}")
-"""
+
 #interrompendo um laço while
 s = 0 
 while True:
@@ -48,3 +48,28 @@ while True:
         break
     s += v
 print(f"Soma: {s}")
+
+"""
+
+#Contagem de cédulas
+valor = int(input("Digite o valor a pagar: "))
+cédulas = 0
+atual = 50
+apagar = valor
+while True:
+    if atual <= apagar:
+        apagar -= atual
+        cédulas += 1
+    else:
+       print(f"{cédulas} cédula(s) de R${atual}")
+       if apagar == 0:
+        break
+       if atual == 50:
+        atual = 20
+       if atual == 20:
+        atual = 10
+       elif atual == 10:
+        atual = 5
+       elif atual == 5:
+        atual = 1
+       cédulas = 0 
