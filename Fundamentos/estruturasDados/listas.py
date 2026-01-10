37640ffdd96c853a798890be38d7d860836edb04
josimarsousa <josimarsousa@gmx.com>
2026-01-10 20:49:40 -0300

    apresentando números

--- Fundamentos/estruturasDados/listas.py
+++ Fundamentos/estruturasDados/listas.py
@@ -21,7 +21,7 @@ while x < 5:
     print(f"soma = {soma}")
     print(f"x = {x}")
 print(f"Média: {soma / x:5.2f}")
-"""
+
 #Calculo da média com notas digitadas pelo usuário
 notas = [0, 0, 0, 0, 0]
 soma = 0
@@ -35,3 +35,15 @@ while x < 5:
     print(f"nota {x+1} = {notas[x]:6.2f}")
     x += 1
 print(f"Média: {soma / x:5.2f}")
+"""
+#apresentaçao de números
+numeros = [0, 0, 0, 0, 0]
+x = 0
+while x < 5:
+    numeros[x] = int(input(f"Digite o número {x+1}: "))
+    x += 1
+while True:
+    escolhido = int(input(f"Que posiçao você quer imprimir? (0 para sair): "))
+    if escolhido == 0:
+        break
+    print(f"Você escolheu o númro: {numeros[escolhido-1]}")
