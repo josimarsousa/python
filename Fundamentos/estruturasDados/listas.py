ff2a6ad722041569d9ff13bc9bd38dafdd4cc5c1
josimarsousa <josimarsousa@gmx.com>
2026-01-09 22:42:07 -0300

    listas

--- Fundamentos/estruturasDados/listas.py
+++ Fundamentos/estruturasDados/listas.py
@@ -1,4 +1,5 @@
 #lista
+"""
 Z = [1,2,3,4,5,6,7,8,9,10]
 print(Z)
 
@@ -9,12 +10,14 @@ print(Z[0])
 Z[0] = 100
 print(Z)
 
-
+"""
 #calculo de media
-notas = [6,7,4,9, 3]
+notas = [6,7,5,8,9]
 soma = 0
 x = 0
 while x < 5:
     soma += notas[x]
     x += 1
-print(f"Média: {soma / x:.2f}")
+    print(f"soma = {soma}")
+    print(f"x = {x}")
+print(f"Média: {soma / x:5.2f}")
