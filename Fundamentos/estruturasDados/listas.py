a90538a796d494bc6a8bd17e9f9a3c93aa9250c0
josimarsousa <josimarsousa@gmx.com>
2026-01-09 22:46:13 -0300

    listas valores digitados pelo usuario

--- Fundamentos/estruturasDados/listas.py
+++ Fundamentos/estruturasDados/listas.py
@@ -10,7 +10,7 @@ print(Z[0])
 Z[0] = 100
 print(Z)
 
-"""
+
 #calculo de media
 notas = [6,7,5,8,9]
 soma = 0
@@ -20,4 +20,18 @@ while x < 5:
     x += 1
     print(f"soma = {soma}")
     print(f"x = {x}")
+print(f"Média: {soma / x:5.2f}")
+"""
+#Calculo da média com notas digitadas pelo usuário
+notas = [0, 0, 0, 0, 0]
+soma = 0
+x = 0
+while x < 5:
+    notas[x] = float(input(f"Digite a nota {x+1}: "))
+    soma += notas[x]
+    x +=  1
+x = 0
+while x < 5: 
+    print(f"nota {x+1} = {notas[x]:6.2f}")
+    x += 1
 print(f"Média: {soma / x:5.2f}")
