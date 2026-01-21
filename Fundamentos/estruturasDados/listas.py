883cc840c0b8f9b85399e565ac26348964e73665
josimarsousa <josimarsousa@gmx.com>
2026-01-21 19:44:10 -0300

    listas-termino programa dicionario op de vendas

--- Fundamentos/estruturasDados/listas.py
+++ Fundamentos/estruturasDados/listas.py
@@ -458,5 +458,25 @@ while True:
 estoque = {
     "maças": [100, 12.30],
     "alface": [500, 0.45],
-    "abacaxi": [200]
+    "abacaxi": [2001, 1.20],
+    "feijao": [100, 1.50]
 }
+venda = [["maças", 2], ["alface", 3], ["abacaxi", 1]]
+total = 0
+print("Vendas:\n")
+for operacao in venda:
+    produto, quantidade = operacao
+    if produto in estoque:
+       preco = estoque[produto][1]
+       custo = preco * quantidade
+       print(f"{produto:12s}: {quantidade:3d} x {preco:6.2f} = {custo:6.2f}")
+       estoque[produto][0] -= quantidade
+       total += custo
+    print(f"Custo total: {total:21.2f}\n")
+    print(f"Estoque: \n")
+    for chave, dados in estoque.items():
+        print("Descrição: ", chave)
+        print("Quantidade: ", dados[0])
+        print(f"Preço: {dados[1]:6.2f}\n")
+        
+        print("")
