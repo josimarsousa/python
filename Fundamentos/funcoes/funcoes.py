2c07ec75fa6ea51d963929144f1ff5ecbe177e73
josimarsousa <josimarsousa@gmx.com>
2026-01-24 13:29:56 -0300

    funcoes-par e impar

--- Fundamentos/funcoes/funcoes.py
+++ Fundamentos/funcoes/funcoes.py
@@ -5,3 +5,16 @@ def soma(a, b):
 print(soma(2, 3))
 print(soma(5, 7))
 print(soma(10, 20))
+
+#verificar se número é par ou impar
+def par_impar(num):
+    if num % 2 == 0:
+        return f"{num} é par"
+    else:
+        return f"{num} é impar"
+
+print(par_impar(2))
+print(par_impar(3))
+print(par_impar(4))
+print(par_impar(5))
+print(par_impar(6))
