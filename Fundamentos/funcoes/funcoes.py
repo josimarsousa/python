5ad5807ff7849d0eca19d36e54cd9a401d1619dd
josimarsousa <josimarsousa@gmx.com>
2026-01-25 22:12:45 -0300

    funcoes-dinamismo no fatorial

--- Fundamentos/funcoes/funcoes.py
+++ Fundamentos/funcoes/funcoes.py
@@ -96,3 +96,18 @@ def fatorial2(n):
 
 numero= int(input(f"Digite um número para calcular o fatorial: "))
 fator2 = fatorial2(numero)    
+
+#funcao fatorial mais dinamica
+print("Fatorial mais dinamica")
+def fatorial3(n):
+    print(f"Calculando o fatorial de {n}")
+    if n == 0 or n ==1:
+        print(f"Fatorial de {n} é 1")
+        return 1
+    else:
+        fat = n * fatorial3(n-1)
+        print(f"Fatorial de {n} é {fat}")
+    return fat
+
+#exemplo de fatorial3
+print(fatorial3(5))
