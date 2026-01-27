47620a15a53a0ba9588619f5ca0f04205f78e059
josimarsousa <josimarsousa@gmx.com>
2026-01-27 11:18:22 -0300

    funções como parâmetro

--- Fundamentos/funcoes/funcoes.py
+++ Fundamentos/funcoes/funcoes.py
@@ -175,3 +175,16 @@ def retangulo(largura, altura, caractere="*"):
 
 #teste
 retangulo(5, 8)
+
+
+#funções como parâmetro
+def soma(a, b):
+    return a+b
+
+def subtracao(a, b):
+    return a-b
+
+def imprime(a, b, foper):
+    print(foper(a, b))
+imprime(5, 4, soma)
+imprime(10, 1, subtracao)
