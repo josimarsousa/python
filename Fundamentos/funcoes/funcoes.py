35b881b8d4d76642bcb1d381c2fefb3720926a79
josimarsousa <josimarsousa@gmx.com>
2026-01-31 09:27:08 -0300

    funcoes-listando em niveis diferentes

--- Fundamentos/funcoes/funcoes.py
+++ Fundamentos/funcoes/funcoes.py
@@ -391,3 +391,13 @@ for numero in L:
         for e in numero:
             print(f" {e} ", end="")
         print()
+
+#imprimindo uma lista de inteiros com m;ultiplos níveis
+def imprime_listas(lista, nivel=0):
+    for x in lista:
+        if isinstance(x, int):
+            print(f"{x:{nivel * 2}}")
+        else:
+            imprime_listas(x, nivel + 1)
+print("Lista em niveis diferentes")
+imprime_listas([1, 2, [3, 4, [5, 6, [7,8,9]], 10]])
