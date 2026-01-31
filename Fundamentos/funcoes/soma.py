65ac62a2dabe049230d8b25dd43947609b3e44d7
josimarsousa <josimarsousa@gmx.com>
2026-01-31 08:43:58 -0300

    funcoes-importando o metodo do modulo

--- Fundamentos/funcoes/soma.py
+++ Fundamentos/funcoes/soma.py
@@ -1,8 +1,16 @@
+"""
 import entrada  #chama funcao valida_inteiro do modulo entrada
 
 L = []
 for n in range(10):
     L.append(entrada.valida_inteiro(f"Digite o um número: ", 0, 20))
 print(f"Soma: {sum(L)}")
+"""
 
+#importando somente o metodo valida_inteiro do modulo entrada
+from entrada import valida_inteiro
 
+L = []
+for n in range(10):
+    L.append(valida_inteiro(f"Digite o um número: ", 0, 20))
+print(f"Soma: {sum(L)}")
