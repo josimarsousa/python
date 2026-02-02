c0e8c0288a6b07821778d87f4a209bde05c12ea1
josimarsousa <josimarsousa@gmx.com>
2026-02-02 12:07:03 -0300

    arquivo-lendo e imprimindo na tela

--- Fundamentos/arquivos/arquivo1.py
+++ Fundamentos/arquivos/arquivo1.py
@@ -1,4 +1,13 @@
+#gerando e granvando um arquivo
+"""
 arquivo = open("números.txt", "w")
 for linha in range(1, 101):
     arquivo.write(f"{linha}\n")
+arquivo.close()
+"""
+
+#lendo e abrindo um arquivo e imprimir suas linhas na tela
+arquivo = open("números.txt", "r")
+for linha in arquivo.readlines():
+    print(linha)
 arquivo.close()
