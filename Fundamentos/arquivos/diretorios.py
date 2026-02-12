91a4f93b5b1dadfc684b470cbea7e737df01e801
josimarsousa <josimarsousa@gmx.com>
2026-02-11 21:02:33 -0300

    arquivo-modulo os.path

--- Fundamentos/arquivos/diretorios.py
+++ Fundamentos/arquivos/diretorios.py
@@ -38,11 +38,19 @@ print(os.listdir("."))
 os.makedirs("avo/pai/filho")
 os.makedirs("avo/mae/filha")
 os.rename("avo/mae/filha", "avo/mae/filho")
-"""
+
 #cria um arquivo e o fecha imediatamente
 open("moribundo.txt", "w").close()
 os.mkdir("vago")
 os.rmdir("vago")
 #os.rmdir("moribundo.txt") = esta linha remove o arquivo criando anteriormente
+"""
 
+#modulo os.path
+import os.path
+for a in os.listdir("."):
+    if os.path.isdir(a):
+        print(f"{a}/")
+    elif os.path.isfile(a):
+        print(a)
 
