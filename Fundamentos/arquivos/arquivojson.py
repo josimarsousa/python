b7fad10f0dc1aafe0a4f2bf6a7c49cb6e7187cfd
josimarsousa <josimarsousa@gmx.com>
2026-02-16 14:19:46 -0300

    arquivo-lendo arquivos e listas

--- Fundamentos/arquivos/arquivojson.py
+++ Fundamentos/arquivos/arquivojson.py
@@ -8,3 +8,11 @@ with Path("dados.json").open() as arquivo:
 print(dados["nome"])
 print(dados["valores"])
 
+#lendo uma lista
+with Path("lista.json").open(encoding="utf-8") as arquivo:
+    turma = json.load(arquivo)
+for aluno in turma:
+    print("Nome: ", aluno["nome"])
+    print("Notas: ", aluno["notas"])
+    print("Média: ", sum(aluno["notas"]) / len(aluno["notas"]))
+    print("-----------------")
