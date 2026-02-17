6b84e72540dca4b256b0f8307fbabdf6063ce3d5
josimarsousa <josimarsousa@gmx.com>
2026-02-17 20:41:40 -0300

    arquivo-lendo arquivos binarios

--- Fundamentos/arquivos/arquivosbinarios.py
+++ Fundamentos/arquivos/arquivosbinarios.py
@@ -7,8 +7,13 @@ def imprime_bytes(imagem, bytes_por_linha=16):
     for b in itertools.batched(imagem, bytes_por_linha):
         hex_view = " ".join([f"{v:02x}" for v in b])
         tview = "".join([chr(v) if chr(v).isprintable() else "." for v in b])
-        print(f"{hex_view} {" " * 3 * (bytes_por_linha - len(b))}{tview}")
+        padding = " " * 3 * (bytes_por_linha - len(b))
+        print(f"{hex_view} {padding}{tview}")
 if __name__ == "__main__":
-    with open(sys.argv[1], "rb") as f:
-        imagem - f.read()
+    if len(sys.argv) < 2:
+        print("Uso: python arquivosbinarios.py <arquivo>")
+        sys.exit(1)
+    caminho = sys.argv[1]
+    with open(caminho, "rb") as f:
+        imagem = f.read()
     imprime_bytes(imagem)
