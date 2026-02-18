16d84736f48dd22263f6c2a877a9e5b81adf33f6
josimarsousa <josimarsousa@gmx.com>
2026-02-17 22:01:33 -0300

    classeseobjetos-demonstracao conta banco

---
+++ Fundamentos/classesObjetos/contas.py
@@ -0,0 +1,30 @@
+class Conta:
+    def __init__(self, clientes, numero, saldo=0):
+        self.saldo = saldo
+        self.clientes = clientes
+        self.numero = numero
+        self.operacoes = []
+        self.deposito(saldo)
+    
+    def resumo(self):
+        print(f"CC Numero: {self.numero}")
+        print(f"Saldo: {self.saldo:10.2f}")
+    
+    def saque(self, valor):
+        if self.saldo >= valor:
+            self.saldo -= valor
+            self.operacoes.append(["SAQUE", valor])
+            print(f"Saque de {valor} realizado com sucesso.")
+        else:
+            print(f"Saldo insuficiente.")
+
+    def deposito(self, valor):
+        self.saldo += valor
+        print(f"Depósito de {valor} realizado com sucesso.")
+        self.operacoes.append(["DEPÓSITO", valor])
+
+    def extrato(self):
+        print(f"EXTRATO CC N {self.numero}\n")
+        for op in self.operacoes:
+            print(f"{op[0]:10s} {op[1]:10.2f}")
+        print(f"Saldo: {self.saldo:10.2f}\n")
--- Fundamentos/classesObjetos/testeclientes.py
+++ Fundamentos/classesObjetos/testeclientes.py
@@ -1,7 +1,18 @@
 from clientes import Cliente
+from contas import Conta
 
-c1 = Cliente("Joao", "123456789")
-c2 = Cliente("Maria", "987654321")
+c1 = Cliente("Joao", "777-1234")
+c2 = Cliente("Maria", "555-4321")
 
-print(c1.nome, c1.telefone)
-print(c2.nome, c2.telefone)
+conta1 = Conta([c1], 1, 1000)
+conta2 = Conta([c2], 2, 500)
+
+conta1.saque(200)
+conta2.deposito(500)
+conta1.saque(150)
+conta2.deposito(95.15)
+conta2.saque(250)
+conta1.extrato()
+conta2.extrato()
+
+
