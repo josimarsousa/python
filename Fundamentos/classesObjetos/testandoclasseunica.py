5222b287ce3d75d00b1efeef43f11bedc796a38f
josimarsousa <josimarsousa@gmx.com>
2026-02-18 21:19:24 -0300

    classeseobjetos-criando e testando lista

---
+++ Fundamentos/classesObjetos/testandoclasseunica.py
@@ -0,0 +1,10 @@
+from listaunica import ListaUnica
+
+lu = ListaUnica(int)
+print(lu)
+print(lu.adiciona(5))
+print(lu.adiciona(3))
+print(lu.adiciona(2))
+print(len(lu))
+for e in lu:
+    print(e)
