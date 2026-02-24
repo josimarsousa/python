9f8dfc274724d306862a56042a66d749cce1748f
josimarsousa <josimarsousa@gmx.com>
2026-02-24 20:19:56 -0300

    bancodados-terminando code alterabrasil.py

--- Fundamentos/bancoDados/alterbrasil.py
+++ Fundamentos/bancoDados/alterbrasil.py
@@ -5,4 +5,11 @@ dados = [["SP", "SE", "SAO PAULO"], ["RJ", "SE", "RIO DE JANEIRO"], ["MG", "SE",
 ["ES", "SE", "ESPIRITO SANTO"], ["BA", "SE", "BAHIA"], ["PE", "SE", "PERNAMBUCO"], ["AL", "SE", "ALAGOAS"],
 ["SE", "SE", "SERGIPE"], ["PI", "SE", "PIAUI"], ["CE", "SE", "CEARA"], ["RN", "SE", "RIO GRANDE DO NORTE"],
 ["PB", "SE", "PARAIBU"], ["MA", "SE", "MARANHAO"],["TO", "SE", "TOCANTINS"], ["AC", "N", "ACRE"], ["RO", "N", "RONDONIA"],
-["AM", "N", "AMAZONAS"]]
+["AM", "N", "AMAZONAS"], ["PA", "N", "PARA"], ["AP", "N", "AMAPA"], ["RR", "N", "RORAIMA"], ["MA", "N", "MARANHAO"],
+["DF", "N", "DISTRITO FEDERAL"]]
+
+with sqlite3.connect("brasil.db") as conexao:
+    conexao.executemany("""update estados
+                        set sigla = ?,
+                        regiao = ?
+                        WHERE nome = ?""", dados)
