f9e30709e2b58d64067236d179a3bdbad04a1ccf
josimarsousa <josimarsousa@gmx.com>
2026-02-23 22:02:48 -0300

    bancodados-alteracao tabela

---
+++ Fundamentos/bancoDados/alterbrasil.py
@@ -0,0 +1,8 @@
+from locale import AM_STR
+import sqlite3
+
+dados = [["SP", "SE", "SAO PAULO"], ["RJ", "SE", "RIO DE JANEIRO"], ["MG", "SE", "MINAS GERAIS"],
+["ES", "SE", "ESPIRITO SANTO"], ["BA", "SE", "BAHIA"], ["PE", "SE", "PERNAMBUCO"], ["AL", "SE", "ALAGOAS"],
+["SE", "SE", "SERGIPE"], ["PI", "SE", "PIAUI"], ["CE", "SE", "CEARA"], ["RN", "SE", "RIO GRANDE DO NORTE"],
+["PB", "SE", "PARAIBU"], ["MA", "SE", "MARANHAO"],["TO", "SE", "TOCANTINS"], ["AC", "N", "ACRE"], ["RO", "N", "RONDONIA"],
+["AM", "N", "AMAZONAS"]]
