efb08b4682a023c92307f71aea31fb593092e2ba
josimarsousa <josimarsousa@gmx.com>
2026-02-23 21:56:06 -0300

    bancodados-alterando tabela

---
+++ Fundamentos/bancoDados/alterandobrasil.py
@@ -0,0 +1,8 @@
+import sqlite3
+
+with sqlite3.connect("brasil.db") as conexao:
+    conexao.execute("""ALTER TABLE estados
+                    add sigla text""")
+
+    conexao.execute("""ALTER TABLE estados
+                    add regiao text""")
