4e1f3822e941753247c48bae88ba4155a0ff852f
josimarsousa <josimarsousa@gmx.com>
2026-02-25 21:53:59 -0300

    bancodados-ajustando parametros

--- Fundamentos/bancoDados/agrupandodados.py
+++ Fundamentos/bancoDados/agrupandodados.py
@@ -3,12 +3,11 @@ import sqlite3
 print("Regiao Número de Estados")
 print("====== =================")
 with sqlite3.connect("brasil.db") as conexao:
-    cursor = conexao.execute("""SELECT regiao, COUNT(*) FROM estados GROUP BY regiao""")
     for regiao in conexao.execute("""
         select regiao, count(*)
         from estados
         group by regiao"""):
-        print(f"{0:6} {1:17}".format(*regiao))        
+        print("{0:6} {1:17}".format(*regiao))        
 
 conexao.commit()
 conexao.close()
