ce8a81fbbcca9cf89f888d3df55b998bbdfdd836
josimarsousa <josimarsousa@gmx.com>
2026-02-25 21:41:44 -0300

    bancodados-modificando tabela estados

--- Fundamentos/bancoDados/alterandobrasil.py
+++ Fundamentos/bancoDados/alterandobrasil.py
@@ -1,11 +1,9 @@
 import sqlite3
 
 with sqlite3.connect("brasil.db") as conexao:
-    conexao.execute("""ALTER TABLE estados
-                    add sigla text""")
+    conexao.execute("""alter table estados add sigla text""")
 
-    conexao.execute("""ALTER TABLE estados
-                    add regiao text""")
+    conexao.execute("""alter table estados add regiao text""")
 
 conexao.commit()
 conexao.close()
