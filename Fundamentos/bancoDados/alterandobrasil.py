c23229a397b377d89e03ada40d26edd07774e3b1
josimarsousa <josimarsousa@gmx.com>
2026-03-02 13:28:43 -0300

    bancodados-ajuste preventivo

--- Fundamentos/bancoDados/alterandobrasil.py
+++ Fundamentos/bancoDados/alterandobrasil.py
@@ -1,9 +1,11 @@
 import sqlite3
 
 with sqlite3.connect("brasil.db") as conexao:
+    # A coluna regiao já foi criada no script brasil.py
+    # conexao.execute("""ALTER TABLE estados ADD regiao text""")
+    
+    # Adicionamos apenas a coluna sigla
     conexao.execute("""ALTER TABLE estados ADD sigla text""")
-
-    conexao.execute("""ALTER TABLE estados ADD regiao text""")
 
 conexao.commit()
 conexao.close()
