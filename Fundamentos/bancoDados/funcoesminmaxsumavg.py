c5cb7b802b011b0c39aa7b602615b17fdb200710
josimarsousa <josimarsousa@gmx.com>
2026-03-03 20:58:40 -0300

    bancodados-calcuo funcao funcoesminmaxsumavg.py pronta

---
+++ Fundamentos/bancoDados/funcoesminmaxsumavg.py
@@ -0,0 +1,19 @@
+import sqlite3
+
+print(f"{'Região':<15} {'Estados':>7} {'Mínima':>13} {'Máxima':>13} {'Média':>13} {'Total':>13}")
+print(f"{'='*15} {'='*7} {'='*13} {'='*13} {'='*13} {'='*13}")
+
+with sqlite3.connect("brasil.db") as conexao:
+    for regiao in conexao.execute("""
+        select regiao, count(*), min(populacao),
+        max(populacao), avg(populacao), sum(populacao)
+        from estados
+        group by regiao"""):
+        print("{0:<15} {1:7d} {2:13,} {3:13,} {4:13,.0f} {5:13,}".format(*regiao))
+    
+    print("-" * 79)
+    dados_brasil = conexao.execute("""
+        select count(*), min(populacao), max(populacao),
+               avg(populacao), sum(populacao) from estados""").fetchone()
+    print("{0:<15} {1:7d} {2:13,} {3:13,} {4:13,.0f} {5:13,}".format(
+        "Brasil", *dados_brasil))
