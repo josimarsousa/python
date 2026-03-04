604cda84990481492f61e1881b8f3f41832a1e36
josimarsousa <josimarsousa@gmx.com>
2026-03-03 21:04:35 -0300

    bancodados-acrescentando a clausula having

--- Fundamentos/bancoDados/funcoesminmaxsumavg.py
+++ Fundamentos/bancoDados/funcoesminmaxsumavg.py
@@ -6,9 +6,9 @@ print(f"{'='*15} {'='*7} {'='*13} {'='*13} {'='*13} {'='*13}")
 with sqlite3.connect("brasil.db") as conexao:
     for regiao in conexao.execute("""
         select regiao, count(*), min(populacao),
-        max(populacao), avg(populacao), sum(populacao)
+        max(populacao), avg(populacao), sum(populacao) as tpop
         from estados
-        group by regiao"""):
+        group by regiao having count(*) > 5 order by tpop desc"""):
         print("{0:<15} {1:7d} {2:13,} {3:13,} {4:13,.0f} {5:13,}".format(*regiao))
     
     print("-" * 79)
