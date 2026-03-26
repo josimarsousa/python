4670fca030bb5b9768e3e2364719043e72296f61
josimarsousa <josimarsousa@gmx.com>
2026-03-25 22:36:17 -0300

    interfacegrafica-class Applicacao iniciada

--- Fundamentos/interfaceGrafica/janela3_usandoclasses.py
+++ Fundamentos/interfaceGrafica/janela3_usandoclasses.py
@@ -1,3 +1,12 @@
 import tkinter as tk
 from tkinter import ttk
 
+class Aplicacao:
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.contador_1 = 0
+        self.contador_2 = 0
+        self.title("Contadores")
+        self.geometry("250x100")
+        self.cria_quadro()
+        
