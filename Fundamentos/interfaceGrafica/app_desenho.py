41dc7efa84b3231a283e2c9d426a23a4ea4d5204
josimarsousa <josimarsousa@gmx.com>
2026-04-03 12:06:59 -0300

    interface grafica-mapeando o mouse

--- Fundamentos/interfaceGrafica/app_desenho.py
+++ Fundamentos/interfaceGrafica/app_desenho.py
@@ -10,13 +10,16 @@ class App(tk.Tk):
         self.title("Desenho")
         self.geometry("800x600")
         self.quadro.pack(expand=True, fill="both")
+        self.cruz = []
+        self.cruz.append(self.canvas.create_line((0,0,0,0), dash=[2,4]))
+        self.cruz.append(self.canvas.create_line((0,0,0,0), dash=[2,4]))
     
     def cria_area_de_desenho(self):
         self.trabalho = ttk.Frame(self.quadro, height=600)
         self.trabalho.grid(column=1, row=0, sticky=tk.NSEW)
         self.quadro.grid_columnconfigure(1, weight=1)
         self.quadro.grid_rowconfigure(0, weight=1)
-        self.canvas = tk.Canvas(self.trabalho, background="white")
+        self.canvas = tk.Canvas(self.trabalho, background="#FFFFFF")
         self.canvas.pack(fill=tk.BOTH, expand=True)
         self.canvas.bind("<Motion>", self.mouse_move)
         self.coordenadas = tk.Label(self.trabalho, text="Mova o mouse")
@@ -28,6 +31,8 @@ class App(tk.Tk):
 
     def mouse_move(self, event):
         self.coordenadas["text"] = f"Mouse x={event.x} y={event.y}"
+        self.canvas.coords(self.cruz[0], event.x, 0, event.x, self.canvas.winfo_height())
+        self.canvas.coords(self.cruz[1], 0, event.y, self.canvas.winfo_width(), event.y)
 
 App().mainloop()
 
