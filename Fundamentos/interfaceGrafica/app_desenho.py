de5b8d29472bff06f4c645f10a036b7fead1c4b7
josimarsousa <josimarsousa@gmx.com>
2026-04-03 21:09:02 -0300

    interfacegrafica-app desenho concluido

--- Fundamentos/interfaceGrafica/app_desenho.py
+++ Fundamentos/interfaceGrafica/app_desenho.py
@@ -1,10 +1,13 @@
 import tkinter as tk
 import tkinter.ttk as ttk
+from tkinter.colorchooser import askcolor
 
 class App(tk.Tk):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.quadro = ttk.Frame(self)
+        self.cor_de_frente = "black"
+        self.cor_de_fundo = ""
         self.cria_barra()
         self.cria_area_de_desenho()
         self.title("Desenho")
@@ -13,12 +16,12 @@ class App(tk.Tk):
         self.cruz = []
         self.cruz.append(self.canvas.create_line((0,0,0,0), dash=[2,4]))
         self.cruz.append(self.canvas.create_line((0,0,0,0), dash=[2,4]))
-        self.cor_de_frente = "black"
         self.estado = 0
         self.xi = None
         self.yi = None
         self.curr_id = 0
         self.ferramenta = self.canvas.create_line
+        
 
     def cria_area_de_desenho(self):
         self.trabalho = ttk.Frame(self.quadro, height=600)
@@ -53,12 +56,24 @@ class App(tk.Tk):
         blimpa = ttk.Button(self.barra, text="Limpa", padding="10", command=self.limpa)
         blimpa.pack()
 
+        self.lfrente = ttk.Label(self.barra, text="Cor de frente")
+        self.lfrente.pack()
+        self.bfrente = tk.Button(self.barra, text="Cor", command=self.cor_frente, bg=self.cor_de_frente)
+        self.bfrente.pack(fill="x")
+        self.lfundo = ttk.Label(self.barra, text="Cor de fundo")
+        self.lfundo.pack()
+        self.bfundo = tk.Button(self.barra, text="Transparente", command=self.cor_fundo, bg=None)
+        self.bfundo.pack(fill="x")
+
     def mouse_move(self, event):
         self.coordenadas["text"] = f"Mouse x={event.x} y={event.y}"
         self.canvas.coords(self.cruz[0], event.x, 0, event.x, self.canvas.winfo_height())
         self.canvas.coords(self.cruz[1], 0, event.y, self.canvas.winfo_width(), event.y)
         if self.estado ==1:
             self.canvas.coords(self.curr_id, self.xi, self.yi, event.x, event.y)
+        tipo = self.canvas.type(self.curr_id)
+        if tipo in ["rectangle", "oval"]:
+            self.canvas.itemconfig(self.curr_id, {"outline": self.cor_de_frente, "fill": self.cor_de_fundo})
 
     def mouse_click(self, event):
         if self.estado == 0:
@@ -87,6 +102,18 @@ class App(tk.Tk):
 
     def limpa(self):
         self.canvas.delete("desenho")
+
+    def cor_fundo(self):
+        cor = askcolor(title="Cor de fundo")
+        self.cor_de_fundo = cor[1] or ""
+        self.bfundo.config(text="Transparente" if self.cor_de_fundo == "" else "",
+            background=self.cor_de_fundo or "SystemButtonFace")
+
+    def cor_frente(self):
+        cor = askcolor(title="Cor de frente")
+        if cor[1]:
+            self.cor_de_frente = cor[1]
+            self.bfrente.config(background=self.cor_de_frente)
 
 App().mainloop()
 
