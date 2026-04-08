d2e678b981b1050cd3db5f71510aa9e70e68ce30
josimarsousa <josimarsousa@gmx.com>
2026-04-07 23:36:53 -0300

    def cria_controles ok

--- Fundamentos/interfaceGrafica/gerenciadorSites/app.py
+++ Fundamentos/interfaceGrafica/gerenciadorSites/app.py
@@ -14,4 +14,31 @@ class App(tk.Tk):
         self.gerente.carrega("dados.json")
         self.mostra_dados()
         self.minimize(self.MIN_X, self.MIN_Y)
+
+    def cria_controles(self):
+        self.quadro = ttk.Frame(self)
+        self.quadro.grid(
+            row=0, column=0, columnspan=2, padx=10, pady=10, sticky="tk.NSEW"
+        )
+        self.grid_rowconfigure(0, weight=1)
+        self.tabela = ttk.Treeview(self.quadro, columns=["url", "categoria", "data", "notas"], show="headings")
+        self.tabela.heading("url", text="URL")
+        self.tabela.heading("categoria", text="Categoria")
+        self.tabela.heading("categoria", anchor="tk.CENTER")
+        self.tabela.heading("data", text="Data")
+        self.tabela.heading("data", anchor="tk.CENTER")
+        self.tabela.heading("notas", text="Notas")
+        self.tabela.grid(
+            row=0,
+            column=0,
+            sticky="tk.NSEW",
+        )
+        self.tabela.config(selectmode="browse")
+        scrollbar = ttk.Scrollbar(self.quadro ,orient=tk.VERTICAL, command=self.tabela.yview)
+        self.tabela.configure(yscroll=scrollbar.set)
+        scrollbar.grid(row=0, column=1, sticky="tk.NSEW")
+        self.quadro.grid_columnconfigure(0, weight=1)
+        self.quadro.grid_rowconfigure(0, weight=1)
+        self.quadro.pack(expand=True, fill=tk.BOTH, padx=10, pady=10)
         
+       
