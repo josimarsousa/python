6da5c5c42f681735c10b7dcf1b790cf1b5e74363
josimarsousa <josimarsousa@gmx.com>
2026-04-08 23:00:37 -0300

    class controles ok

--- Fundamentos/interfaceGrafica/gerenciadorSites/data.py
+++ Fundamentos/interfaceGrafica/gerenciadorSites/data.py
@@ -20,4 +20,12 @@ class Data(ttk.Frame):
     def get(self):
         return f"{self.dia.get()}-{self.mes.get()}-{self.ano.get()}"
 
-    
+    def cria_controles(self):
+        self.c_dia = ttk.Combobox(self, textvariable=self.dia, width=3, values=[f"{d:02d}" for d in range(1, 32)],
+        state="readonly")
+        self.c_dia.pack(side=tk.LEFT)
+        self.c_mes = ttk.Combobox(self, textvariable=self.mes, values=[f"{m:02d}" for m in range(1, 13)], width=3, state="readonly")
+        self.c_mes.pack(side=tk.LEFT)
+        self.c_ano = ttk.Combobox(self, textvariable=self.ano, values=[f"{a:04d}" for a in range(self.min_ano, self.max_ano+1)], width=4, state="readonly")
+        self.c_ano.pack(side=tk.LEFT)
+       
